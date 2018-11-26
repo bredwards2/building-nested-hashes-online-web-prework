41d61cb9eb391abab1ac3a6c106632e5a42faae9
Britny Edwards <bredwards2@gmail.com>
2018-11-26 00:35:26 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,5 +1,5 @@
 def first_challenge
    epic_tragedy = {
- montague: ["Patriach", "Matriach", "Rose"],
- dev_team: ["Jonas", "Logan", "Amanda", "Seiji", "Kate", "Spencer"],
+ montague: ["Patriach", "Matriach", "Hero", "Hero's Friends"],
+ capulet: ["Jonas", "Logan", "Amanda", "Seiji", "Kate", "Spencer"],
  students: ["Sarah", "you", "John", "David"]
